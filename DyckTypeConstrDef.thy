62a21771da763bcfb9d059d11995c24d94210a23
José Manuel Rodríguez Caballero <37361666+josephcmac@users.noreply.github.com>
2018-10-25 20:22:37 -0400

    Add files via upload

---
+++ DyckTypeConstrDef.thy
@@ -0,0 +1,50 @@
+(*
+Title: Constructive definition of the Dyck type of a number
+Author: Jose Manuel Rodriguez Caballero
+
+We define Dyck n to be the Dyck type of n.
+
+References
+
+@inproceedings{caballero2017symmetric,
+  title={Symmetric Dyck Paths and Hooley’s $$$\backslash$varDelta $$ $\Delta$-Function},
+  author={Caballero, Jos{\'e} Manuel Rodr{\'\i}guez},
+  booktitle={International Conference on Combinatorics on Words},
+  pages={252--261},
+  year={2017},
+  organization={Springer}
+}
+
+(This code  was verified in Isabelle2018)
+*)
+
+theory DyckTypeConstrDef
+
+imports Complex_Main
+
+begin
+
+fun SchroederCHR :: \<open>nat \<Rightarrow> nat \<Rightarrow> int\<close> where
+\<open>SchroederCHR n k = 
+      (if k dvd (2*n) \<and> (odd k) then 1 
+ else (if k dvd (2*n) \<and> odd ((2*n)div k) then -1 
+else 0 ))\<close>
+
+fun preSchroeder :: \<open>nat \<Rightarrow> nat \<Rightarrow> int list\<close> where
+\<open>preSchroeder n 0 = []\<close> |
+\<open>preSchroeder n (Suc k) = (preSchroeder n k) @ [SchroederCHR n (Suc k)]\<close> 
+
+fun Schroeder :: \<open>nat  \<Rightarrow> int list\<close> where
+\<open>Schroeder n = preSchroeder n (2*n)\<close> 
+
+fun Ignore :: \<open>'a list \<Rightarrow> 'a \<Rightarrow> 'a list\<close> where
+\<open>Ignore [] x = []\<close>|
+\<open>Ignore (b # L) x = 
+  (if b = x then Ignore L x else b # (Ignore L x))\<close>
+
+text {* Dyck n is the Dyck type of n *}
+fun Dyck :: \<open>nat  \<Rightarrow> int list\<close> where
+\<open>Dyck n = Ignore (Schroeder n) (0::int)\<close> 
+
+
+end
